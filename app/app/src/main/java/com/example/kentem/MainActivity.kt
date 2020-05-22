7cf0a0fc23006f9288578442514712603b9216ee
Raphael Ponce <raphael.fleury@b2wdigital.com>
2020-05-22 10:18:06 -0300

    Criando bottom navigation

---
+++ app/app/src/main/java/com/example/kentem/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kentem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        
+    }
+}
