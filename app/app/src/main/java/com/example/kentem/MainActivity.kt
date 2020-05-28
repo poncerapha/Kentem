3c214f9c16e74828ab64b1c3311692f59a7cae5d
Raphael Ponce <raphafleury123@gmail.com>
2020-05-27 21:23:46 -0300

    Adicionando transicao entre fragments

--- app/app/src/main/java/com/example/kentem/MainActivity.kt
+++ app/app/src/main/java/com/example/kentem/MainActivity.kt
@@ -2,17 +2,56 @@ package com.example.kentem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.kentem.fragment.*
 import com.google.android.material.bottomnavigation.BottomNavigationMenu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home_page -> {
+                    replaceFragment(HomeFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.search_page -> {
+                    replaceFragment(SearchFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.my_ad_page -> {
+                    replaceFragment(MyAnnouncementsFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.message_page -> {
+                    replaceFragment(MessageFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.my_account_page -> {
+                    replaceFragment(MyAccountFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_navigation)
-
-        
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(HomeFragment())
     }
+
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_fragment, fragment)
+        fragmentTransaction.commit()
+    }
+
+
 }
